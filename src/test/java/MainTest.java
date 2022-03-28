a6f7c200835f3cd0acbaabd59e94dc7890567c53
SariGer <sarifalk29@gmail.com>
2022-03-28 15:09:36 +0300

    update

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -12,7 +12,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
